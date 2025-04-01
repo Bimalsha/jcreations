0f4d5b1cf286c4fcaf603b0fd34a1e50e3ebf48c
Tharindu Geeshan <mastermaxx2002@gmail.com>
2025-04-01 21:17:49 +0530

    1st desgin
    init

--- src/client/pages/Cart.jsx
+++ src/client/pages/Cart.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import BottomNavigator from "../component/BottomNavigator.jsx";
 import Header from "../component/Header.jsx";
+import CheckoutPopup from "../component/utils/checkout-popup.jsx";
 
 function Cart() {
     return (
         <div>
             <Header/>
             <BottomNavigator/>
+            <CheckoutPopup/>
             <div>Cart</div>
         </div>
     )
