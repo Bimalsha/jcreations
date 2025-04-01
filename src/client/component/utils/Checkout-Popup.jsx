86612876748aab201e03557c52b007f20bb080f6
Tharindu Geeshan <mastermaxx2002@gmail.com>
2025-04-01 21:45:52 +0530

    commint

--- src/client/component/utils/Checkout-Popup.jsx
+++ src/client/component/utils/Checkout-Popup.jsx
@@ -5,7 +5,7 @@ const Login = () => {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-4 lg:px-6">
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white py-8 px-4 sm:rounded-lg sm:px-10 shadow-[5px_5px_15px_-3px_rgba(247,163,19,0.3)]">
+        <div className="bg-white py-8 px-4 sm:rounded-2xl sm:px-4 shadow-[5px_5px_15px_-3px_rgba(247,163,19,0.3)]">
           <div className="flex justify-between items-start mb-4">
             <div className="w-8"></div>
             <div className="flex items-center justify-center">
@@ -19,7 +19,7 @@ const Login = () => {
           <div className="space-y-3">
             <div>
               <button
-                className="w-full flex justify-center py-4 px-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#000F20] hover:bg-[#F7A313] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full flex justify-center py-4 px-2 border border-transparent rounded-xl shadow-sm text-sm font-medium text-white bg-[#000F20] hover:bg-[#F7A313] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Login for the Best Experience
               </button>
@@ -27,7 +27,7 @@ const Login = () => {
 
             <div>
               <button
-                className="w-full flex justify-center py-4 px-2 border border-yellow-500 rounded-md shadow-sm text-sm font-medium text-yellow-500 bg-none hover:bg-yellow-500 hover:text-white focus:outline-yellow-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                className="w-full flex justify-center py-4 px-2 border border-yellow-500 rounded-xl shadow-sm text-sm font-medium text-yellow-500 bg-none hover:bg-yellow-500 hover:text-white focus:outline-yellow-500 focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
               >
                 Checkout without login
               </button>
