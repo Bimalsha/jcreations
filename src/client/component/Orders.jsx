ddfefe4b0c9c2c646185257497156cd9740fcdc7
Matheesha1000D <149418673+Matheesha1000D@users.noreply.github.com>
2025-04-06 11:49:29 +0530

    oders page create

---
+++ src/client/component/Orders.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Newitem from "./utils/Newitem.jsx";
+import Orderitem from "./utils/Orderitem.jsx";
+
+
+function Orders() {
+    return (
+        <>
+            <Orderitem/>
+        </>
+    )
+}
+
+export default Orders
